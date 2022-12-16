e17191719b7ed43888e7971ec2424632ea44bd87
alohathdz <57053156+alohathdz@users.noreply.github.com>
2022-12-16 12:59:51 +0700

    เพิ่มเมนูแฟ้มบน navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,6 +73,7 @@
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('user.profile', Auth::user()->id) }}">ข้อมูลส่วนตัว</a>
+                                <a class="dropdown-item" href="{{ route('folder.index') }}">แฟ้ม</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">ออกจากระบบ</a>
 
