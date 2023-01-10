0929edfebad5dbaf54e4f40fd140a675a2c92f75
alohathdz <57053156+alohathdz@users.noreply.github.com>
2023-01-10 20:43:50 +0700

    ลบ device ios

--- app/Helper.php
+++ app/Helper.php
@@ -92,5 +92,5 @@ function line($message, $LINE_TOKEN)
 #เช็ค Device Mobile
 function isMobile()
 {
-    return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $_SERVER["HTTP_USER_AGENT"]);
+    return preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $_SERVER["HTTP_USER_AGENT"]);
 }
