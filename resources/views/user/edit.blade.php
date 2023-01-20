73f2f8d25912a0e753064dadf125bde012fa751f
alohathdz <57053156+alohathdz@users.noreply.github.com>
2023-01-20 15:15:04 +0700

    แก้ type edit user เป็น password

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -46,7 +46,7 @@
                     <div class="row mb-3">
                         <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
                         <div class="col-md-5">
-                            <input type="text" class="form-control @error('password') is-invalid @enderror"
+                            <input type="password" class="form-control @error('password') is-invalid @enderror"
                                 id="password" name="password" value="{{ old('password') }}" autocomplete="password">
 
                             @error('password')
